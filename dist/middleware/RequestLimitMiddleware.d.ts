2adac463f5e7bca2699f62c08f5da4b163a3977a
Yahya Alataş <yahya.alatas@outlook.com>
2024-02-16 22:26:51 +0300

    Delete dist directory

--- dist/middleware/RequestLimitMiddleware.d.ts
+++
@@ -1,6 +0,0 @@
-import { NestMiddleware } from "@nestjs/common";
-import { Response, NextFunction } from "express";
-export declare class RequestLimitMiddleware implements NestMiddleware {
-    private userRequestCounts;
-    use(req: any, res: Response, next: NextFunction): Response<any, Record<string, any>>;
-}
